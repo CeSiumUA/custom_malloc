ee62ef96ccea8f80db5e2c35011ff3b871633705
CeSiumUA <fedir.katushonok@gmail.com>
2022-12-01 09:26:25 +0200

    implemented free and malloc

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ struct block_metadata{
 void *malloc(size_t size);
 struct block_metadata *get_free_block(struct block_metadata **previous, size_t size);
 
-void *global_current_block = NULL;
+void *global_base_block = NULL;
 
 int main(void){
     uint32_t *mem = malloc(sizeof(uint32_t) * 11);
@@ -30,31 +30,48 @@ int main(void){
 }
 
 void *malloc(size_t size){
-    void *heapPointer = sbrk(0);
+    struct block_metadata *block;
 
-    void *requested = sbrk(size);
-
-    if(requested == (void*) -1){
+    if(size <= 0){
         return NULL;
     }
-    else{
-        assert(heapPointer == requested);
-        return heapPointer;
+
+    if(global_base_block){
+        block = allocate_space(NULL, size);
+        if(!block){
+            return NULL;
+        }
+        global_base_block = block;
     }
+    else{
+        struct block_metadata *last = global_base_block;
+        block = get_free_block(&last, size);
+        if(!block){
+            block = allocate_space(last, size);
+            if(!block){
+                return NULL;
+            }
+        }
+        else{
+            block -> free = 0;
+        }
+    }
+
+    return (block + 1);
 }
 
-struct block_metadata *get_free_block(struct block_metadata **previous, size_t size){
-    struct block_metadata *current = global_current_block;
+struct block_metadata *get_free_block(struct block_metadata **last, size_t size){
+    struct block_metadata *current = global_base_block;
 
     while(current && !(current -> free && current -> size >= size)){
-        *previous = current;
+        *last = current;
         current = current -> next;
     }
 
     return current;
 }
 
-struct block_metadata *allocate_space(struct block_metadata *previous, size_t size){
+struct block_metadata *allocate_space(struct block_metadata *last, size_t size){
     struct block_metadata *block;
     block = sbrk(0);
 
@@ -65,13 +82,27 @@ struct block_metadata *allocate_space(struct block_metadata *previous, size_t si
         return NULL;
     }
 
-    if(previous){
-        previous -> next = block;
+    if(last){
+        last -> next = block;
     }
 
     block -> size = size;
     block -> next = NULL;
     block -> free = 0;
-    
+
     return block;
+}
+
+struct block_metadata *get_segment_block_metadata(void *ptr){
+    return (struct block_metadata *)ptr - 1;
+}
+
+void free(void *ptr){
+    if(!ptr){
+        return;
+    }
+
+    struct block_metadata *block_ptr = get_segment_block_metadata(ptr);
+    assert(block_ptr -> free == 0);
+    block_ptr -> free = 1;
 }
